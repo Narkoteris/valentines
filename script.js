e6f7a594579430555d747cce81b52eca5e8ec715
Narkoteris <159181845+Narkoteris@users.noreply.github.com>
2024-02-06 18:28:26 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    document.getElementById('response').innerText = 'Yes! Happy Valentine\'s Day!';
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    alert('I will not take no for an answer.');
+});
